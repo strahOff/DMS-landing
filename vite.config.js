d3597c6cd3b9083e0eb0c2a65d94ea78c811fd62
Artem <artm.koshurnikov00@mail.ru>
2026-07-25 00:13:33 +0300

    initial commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  base: "/dms-landing/",
+  base: "/DMS-landing/",
   plugins: [react(), tailwindcss()],
 });
